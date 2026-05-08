c2dcdd1b2360dffd31d5660f22daf57894cec8b2
Florb11 <florbergman11@gmail.com>
2026-05-08 18:25:19 -0300

    conexión a la bd

--- backend/app.py
+++ backend/app.py
@@ -1,19 +1,51 @@
+import os
 from flask import Flask, jsonify
 from flask_cors import CORS
 from dotenv import load_dotenv
+from sqlalchemy import text
+from db import db
 
 load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
 
+DB_HOST = os.getenv("DB_HOST")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_NAME = os.getenv("DB_NAME")
+DB_PORT = os.getenv("DB_PORT")
+
+app.config["SQLALCHEMY_DATABASE_URI"] = (
+    f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+)
+
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db.init_app(app)
+
+
 @app.route("/")
 def home():
     return jsonify({"mensaje": "Backend de Trukly funcionando"})
 
+
 @app.route("/api/test")
 def test():
     return jsonify({"mensaje": "Conexión frontend-backend OK"})
+
+
+@app.route("/api/db-test")
+def db_test():
+    try:
+        db.session.execute(text("SELECT 1"))
+        return jsonify({"mensaje": "Conexión con MySQL OK"})
+    except Exception as error:
+        return jsonify({
+            "mensaje": "Error al conectar con MySQL",
+            "error": str(error)
+        }), 500
+
 
 if __name__ == "__main__":
     app.run(debug=True)
