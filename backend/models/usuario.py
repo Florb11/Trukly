a6b153043e7ff1963353bd9af25fc1055680a8b1
Florb11 <florbergman11@gmail.com>
2026-05-14 13:56:00 -0300

    Script de la db y cree modelo usuario

---
+++ backend/models/usuario.py
@@ -0,0 +1,21 @@
+from db import db
+
+
+class Usuario(db.Model):
+    __tablename__ = "Usuario"
+
+    id_usuario = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(30), nullable=False, unique=True)
+    password = db.Column(db.String(255), nullable=False)
+    nombre = db.Column(db.String(30), nullable=False)
+    apellido = db.Column(db.String(30), nullable=False)
+    estado = db.Column(db.String(45), nullable=False)
+
+    def to_dict(self):
+        return {
+            "id_usuario": self.id_usuario,
+            "username": self.username,
+            "nombre": self.nombre,
+            "apellido": self.apellido,
+            "estado": self.estado
+        }
