9f99ff34496fd54d24f5c292e3328e8f1aa286d3
Florb11 <florbergman11@gmail.com>
2026-05-15 17:49:29 -0300

    Clase Usuario y herencia

---
+++ backend/src/OperadorLogistico.py
@@ -0,0 +1,22 @@
+from src.Usuario import Usuario 
+
+
+class OperadorLogistico(Usuario):
+    def __init__(
+        self,
+        idUsuario,
+        username,
+        password,
+        nombre,
+        apellido,
+        estado,
+        legajo,
+        sector,
+        viajesGestionados=None,
+        camionesAsignados=None,
+    ):
+        super().__init__(idUsuario, username, password, nombre, apellido, estado)
+        self.legajo = legajo
+        self.sector = sector
+        self.viajesGestionados = viajesGestionados or []
+        self.camionesAsignados = camionesAsignados or []
