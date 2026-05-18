0d2037798bed5307f7276cb066f33354c9e54ddc
Florb11 <florbergman11@gmail.com>
2026-05-18 17:49:14 -0300

    registro chofer con bcrypt, jwt token
    falta conectar LoginPage.jsx con el endpoint

--- backend/routes/auth_routes.py
+++ backend/routes/auth_routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint
-from controllers.auth_controller import registrar_chofer
+from controllers.auth_controller import registrar_chofer, login
+
 
 auth_routes = Blueprint("auth_routes", __name__)
 
@@ -8,3 +9,8 @@ auth_routes = Blueprint("auth_routes", __name__)
 def ruta_registrar_chofer():
     # llamo al controller para registrar un chofer pendiente
     return registrar_chofer()
+
+@auth_routes.route("/api/auth/login", methods=["POST"])
+def ruta_login():
+    # llamo al controller para iniciar sesion
+    return login()
