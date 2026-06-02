55f27bf7855527263ca68fc9afd8b33e89643b46
Juan Del Pozo <juandelpozo412@gmail.com>
2026-06-02 14:36:17 -0300

    Correccion

--- backend/routes/chofer_routes.py
+++ backend/routes/chofer_routes.py
@@ -3,20 +3,14 @@ from controllers.chofer_controller import ChoferController
 
 chofer_routes = Blueprint("chofer_routes", __name__)
 
-
 @chofer_routes.route("/api/choferes", methods=["GET"])
 def ruta_listar_choferes():
-# Llamo al método estático listar_choferes de la clase ChoferController
     return ChoferController.listar_choferes()
-
 
 @chofer_routes.route("/api/choferes/<int:id_usuario>", methods=["GET"])
 def ruta_obtener_chofer(id_usuario):
-# Llamo al método estático obtener_chofer pasando el ID
     return ChoferController.obtener_chofer(id_usuario)
-
 
 @chofer_routes.route("/api/choferes", methods=["POST"]) 
 def ruta_crear_chofer():
-# Llamo al controller para crear un chofer nuevo
     return ChoferController.crear_chofer()
