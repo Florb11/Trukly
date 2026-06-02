d9924c61dd4bf9499d997cb3ec41597ec4e5c08e
Juan Del Pozo <juandelpozo412@gmail.com>
2026-06-02 14:31:26 -0300

    ControllerChofer Class
    Adapte el controlador de chofer a una clase

--- backend/controllers/chofer_controller.py
+++ backend/controllers/chofer_controller.py
@@ -3,46 +3,41 @@ from db_instance import db
 
 from models.chofer_model import ChoferModel
 
+class ChoferController:
 
-# funcion para listar todos los choferes guardados en la bd
-def listar_choferes():
+    @staticmethod
+    def listar_choferes():
+        # funcion para listar todos los choferes guardados en la bd
+        choferes = ChoferModel.query.all()
+        return jsonify([chofer.to_dict() for chofer in choferes])
 
-    choferes = ChoferModel.query.all()
+    @staticmethod
+    def obtener_chofer(id_usuario):
+        # Busco chofer por id
+        chofer = ChoferModel.query.get(id_usuario)
 
-    return jsonify([chofer.to_dict() for chofer in choferes])
+        if chofer is None:
+            return jsonify({"mensaje": "Chofer no encontrado"}), 404
 
+        return jsonify(chofer.to_dict())
 
-def obtener_chofer(id_usuario):
-    # Busco chofer por id
-    chofer = ChoferModel.query.get(id_usuario)
+    @staticmethod
+    def crear_chofer():
+        # funcion para crear choferes
+        datos = request.get_json()
 
-    if chofer is None:
-        return jsonify({"mensaje": "Chofer no encontrado"}), 404
+        nuevo_chofer = ChoferModel(
+            Usuario_idUsuario=datos["Usuario_idUsuario"],
+            legajo=datos["legajo"],
+            vencimientoLicencia=datos["vencimientoLicencia"],
+            licencia=datos["licencia"],
+        )
 
-    return jsonify(chofer.to_dict())
+        db.session.add(nuevo_chofer)
+        db.session.commit()
 
-
-# funcion para crear choferes
-
-
-def crear_chofer():
-
-    datos = request.get_json()
-
-    nuevo_chofer = ChoferModel(
-        Usuario_idUsuario=datos["Usuario_idUsuario"],
-        legajo=datos["legajo"],
-        vencimientoLicencia=datos["vencimientoLicencia"],
-        licencia=datos["licencia"],
-    )
-
-    db.session.add(nuevo_chofer)
-    db.session.commit()
-
-    # retorna el mensaje , y sino el error en este caso 201
-    return (
-        jsonify(
-            {"mensaje": "Chofer creado correctamente", "chofer": nuevo_chofer.to_dict()}
-        ),
-        201,
-    )
+        # retorna el mensaje , y sino el error en este caso 201
+        return jsonify({
+            "mensaje": "Chofer creado correctamente", 
+            "chofer": nuevo_chofer.to_dict()
+        }), 201
